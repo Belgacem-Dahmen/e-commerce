0f2a399df2a57bf48b98755e770d3e3199d3ec94
Belgacem-Dahmen <belgacem.dahmen@gmail.comn>
2022-09-26 15:42:46 +0200

    PHP

---
+++ includes/functions.php
@@ -0,0 +1,22 @@
+
+
+  <?php
+            
+            $servername ='localhost';
+            $username = 'admin';
+            $password = 'admin';
+            $dbName ='ipda';
+            
+            connectDb($servername,$username,$password,$dbName);
+            
+            function connectDb($servername,$username,$password,$dbName){
+            $mysqli = new mysqli($servername,$username,$password,$dbName);
+
+            // Check connection
+            if ($mysqli -> connect_errno) {
+            echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+            exit();
+            } 
+            }
+
+        ?>
